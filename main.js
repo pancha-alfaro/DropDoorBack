96e89b4e308d64290a9f3b7665a212dd0d3e063d
pancha-alfaro <papancha.alfaro.h@gmail.com>
2023-06-01 19:32:48 -0500

    se agrega product, model, routes de productos y se crea una segunda coneccion a la api

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
 //Se guarda en la constante express la libreria express
 const express = require("express");
-
+const cors = require('cors');
 
 //Se guardan en la constante app todos los metodos de la libreria express
 const app = express();
 
-//Creacion de la constante puerto
+//Creación de la constante puerto
 const port = 4000;
 
 
-//creacion de la constante userRoutes
+//creacion de la constante userRoutes y productsRoutes
 const userRoutes = require("./routes/userRoutes");
+const productsRoutes = require("./routes/productsRoutes");
 
 //Se agrega middleware para habilitar el análisis y conversion de data JSON en requests.
 app.use(express.json());
+app.use(cors());
 
-//ruta para acceso a la infornacion de la base de datos
+//ruta para acceso a la información de la base de datos
 app.use('/api/users', userRoutes);
+app.use('/api/products', productsRoutes);
 
 //Se empieza un servidor en el puerto especificado y se hace un log con un mensaje que contiene la URL del servidor.
 app.listen(port, () => {console.log("El servidor se ejecuta en http://localhost:" + port)})
---
+++ models/productsModel.js
@@ -0,0 +1,25 @@
+//Se importa la libreria Mongoose. 
+const mongoose = require("mongoose");
+
+
+//Variable Uri almacena la string conection a la base de datos de MongoDB. 
+const Uri = "mongodb+srv://panchaalfaroh:MPo5CU9ig2FDhE0J@users.lialtqx.mongodb.net/Users";
+
+//Se establece la conección a la base de datos, y se hace un log en la consola si es que tuvo exito o si hay algun error. 
+mongoose.connect(Uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("Se conecto con base de datos"))
+.catch(err => console.log("Error de conexion con la base de datos", err));
+
+
+//Se crea un Schema detallando sus propiedades y sus respectivos tipos. 
+const productSchema = new mongoose.Schema({
+    name: {type:String},
+    price: {type:Number},
+    restaurant: {type:String}
+});
+
+//Código que nos permite exportar el model 'Products' en base a productSchema. Permitiendo la interacción de 'Products' con la base de datos. 
+module.exports = mongoose.model('Products', productSchema);
